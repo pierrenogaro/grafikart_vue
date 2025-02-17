ee986abb11129e10d7764395708355e2d1126bf5
Pierre <pierre.nogaro@gmail.com>
2025-02-17 12:04:21 +0100

    TD : Liste taches

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,68 @@
 <template>
-<ul>
-  <li>
-    {{person.firstName}}
-    {{person.lastName}}
-    {{person.age}}
-  </li>
-</ul>
-  <button @click.prevent="randomAge">changer age</button>
+  <form action="" @submit.prevent="addTodo">
+    <fieldset role="group">
+      <input type="text"
+      v-model="newTodo"
+      placeholder="Tâche à effectuer">
+      <button :disabled="newTodo.length === 0">Ajouter la tache</button>
+    </fieldset>
+  </form>
+
+  <div v-if="todos.length === 0"> Vous n'avez pas de tache à faire</div>
+  <div v-else>
+    <ul>
+      <li v-for="todo in todos"
+      :key="todo.date"
+      :class="{completed : todo.completed }"
+      >
+        <label>
+          <input type="checkbox" v-model="todo.completed">
+          {{todo.title}}
+        </label>
+      </li>
+    </ul>
+    <div>
+      <label for="">
+        <input type="checkbox" v-model="hideCompleted">
+        Masquer les tâches complétées
+      </label>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
-const person = ref({
-  firstName: "John",
-  lastName: "Doe",
-  age: 20,
-})
-
-const randomAge = () => {
-  person.value = {
-    ...person.value,
-    age : Math.round(Math.random() * 100)
-  }
+const newTodo = ref([])
+const hideCompleted = ref(false);
+const todos = ref([{
+  title : 'Tache de test',
+  completed: true,
+  date: 1,
+}, {
+    title : 'Tache à faire',
+    completed: false,
+    date: 2,
+  }])
+const addTodo = () => {
+  todos.value.push({
+    title : newTodo.value,
+    completed: true,
+    date : Date.now()
+  })
+  newTodo.value = ''
 }
+
+const sortedTodos = () => {
+  return todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+}
+
 </script>
 
 
 <style>
+.completed {
+  opacity: .5;
+  text-decoration: line-through;
+}
 
 </style>
